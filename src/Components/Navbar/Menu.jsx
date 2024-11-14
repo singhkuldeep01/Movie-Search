86db67dc9878dbe98a385be658a0c6a357fc12b9
Kuldeeprime01 <7351320178k@gmail.com>
2024-11-15 02:18:28 +0530

    code added

--- src/Components/Navbar/Menu.jsx
+++ src/Components/Navbar/Menu.jsx
@@ -21,7 +21,7 @@ function Menu({ setIsMenuOpen }) {
       <p
         onClick={() => {
           setIsMenuOpen(false);
-          navigate('/search');
+          navigate('/');
         }}
         className='px-4 py-2 rounded-lg w-full md:w-auto cursor-pointer transition duration-200 
                    hover:bg-slate-200 dark:hover:bg-slate-600 dark:hover:text-white
